3659b762029f414e8d974f4916ef8d74c268713c
Therion95 <piotrpawelczyk95@gmail.com>
2022-02-13 14:52:18 +0100

    Created base for database migration.

---
+++ FlowMeter.Data/FlowMeterDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FlowMeter.Domain;
+using FlowMeter.Domain.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowMeter.Data
+{
+    public class FlowMeterDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Localization> Localizations { get; set; }
+        public DbSet<Measurement> Measurements { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Shape> Shapes { get; set; } // Nie wiem czy to potrzebne, Asia zerknij.
+        public DbSet<Circle> Circles { get; set; }
+        public DbSet<Trapeze> Trapezes { get; set; }
+        public DbSet<Rectangle> Rectangles { get; set; }
+
+
+        public FlowMeterDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        
+    }
+}
