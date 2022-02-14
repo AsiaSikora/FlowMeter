69b425a0f889452e4de606c1493411674cf73da5
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-02-14 16:53:25 +0100

    UnitOfWork with interface created

---
+++ FlowMeter.DataManipulation/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using FlowMeter.Domain;
+
+namespace FlowMeter.DataManipulation
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<User> Users { get; }
+
+        void Save();
+    }
+}
---
+++ FlowMeter.DataManipulation/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using FlowMeter.Data;
+using FlowMeter.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowMeter.DataManipulation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FlowMeterDbContext _context;
+
+        public UnitOfWork(FlowMeterDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<User> Users => new GenericRepository<User>(_context);
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
