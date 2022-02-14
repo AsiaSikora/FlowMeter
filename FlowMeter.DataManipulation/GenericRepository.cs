12f4796556e57742f026aa7c665b67d3b38c51f7
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-02-14 16:53:05 +0100

    GenericRepository with interface created

---
+++ FlowMeter.DataManipulation/GenericRepository.cs
@@ -0,0 +1,96 @@
+﻿using FlowMeter.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FlowMeter.DataManipulation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly FlowMeterDbContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(FlowMeterDbContext context)
+        {
+            _context = context;
+            _db = context.Set<T>();
+        }
+
+        //public PagedList<T> GetAllParams(RequestParameters requestParameters)
+        //{
+        //    return PagedList<T>.ToPagesList(_db, requestParameters.PageNumber, requestParameters.PageSize);
+        //}
+
+        public List<T> GetAll(Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return query.AsNoTracking().ToList();
+        }
+
+        public T Get(Expression<Func<T, bool>> expression = null,
+            List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return query.AsNoTracking().FirstOrDefault(expression);
+        }
+
+        public void Add(T entity)
+        {
+            _db.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _db.AddRange(entities);
+        }
+
+        public void Remove(int id)
+        {
+            var entity = _db.Find(id);
+            _db.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _db.RemoveRange(entities);
+        }
+
+        public void Modify(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
