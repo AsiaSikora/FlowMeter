d3256779ade7165af3ac903dc46a7177741dae62
Therion95 <piotrpawelczyk95@gmail.com>
2022-02-15 21:16:04 +0100

    Created repository for Devices

---
+++ FlowMeter.DataManipulation/Repositories/DeviceRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FlowMeter.Data;
+using FlowMeter.Domain;
+
+namespace FlowMeter.DataManipulation.Repositories
+{
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(FlowMeterDbContext context) : base(context)
+        {
+            
+        }
+
+
+        public List<Device> GetAllDevicesWithUsersAndSurveys(Expression<Func<Device, bool>> expression = null, 
+            Func<IQueryable<Device>, IOrderedQueryable<Device>> orderBy = null)
+        {
+            return base.GetAll(expression, orderBy, new List<string>() { "Users" });
+        }
+    }
+}
---
+++ FlowMeter.DataManipulation/Repositories/IDeviceRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FlowMeter.Domain;
+
+namespace FlowMeter.DataManipulation.Repositories
+{
+    public interface IDeviceRepository : IGenericRepository<Device>
+    {
+        List<Device> GetAllDevicesWithUsersAndSurveys(Expression<Func<Device, bool>> expression = null, 
+            Func<IQueryable<Device>, IOrderedQueryable<Device>> orderBy = null);
+    }
+}
