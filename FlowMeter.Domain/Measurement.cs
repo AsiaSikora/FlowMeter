4f98297e68ba0b77de732719ab0e93137a0e0fb0
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-02-17 18:52:52 +0100

    Localization and Measurement domain classes refactor

--- FlowMeter.Domain/Measurement.cs
+++ FlowMeter.Domain/Measurement.cs
@@ -8,11 +8,10 @@ namespace FlowMeter.Domain
         public int Id { get; set; }
         public DateTime Time { get; set; }
         public string Battery { get; set; }
-        public double? Pressure { get; set; }
-        public double? Temperature { get; set; }
-        public double? CurrentFlow { get; set; }
-        public double? AverageFlow { get; set; }
-        public double? MedianFlow { get; set; }
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+        public double CurrentFlow { get; set; }
+        public double AverageFlow { get; set; }
         public bool IsSpecialPoint { get; set; }
         public Survey Survey { get; set; }
         [ForeignKey(nameof(Survey))]
