628c0e64d0a172b8df394652347fc5e46b9c2c1f
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-02-26 16:28:00 +0100

    Localization profile added

---
+++ FlowMeter.API/Configuration/Profiles/LocalizationProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FlowMeter.API.Models.Localization;
+using FlowMeter.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowMeter.API.Configuration.Profiles
+{
+    public class LocalizationProfile : Profile
+    {
+        public LocalizationProfile()
+        {
+            CreateMap<Localization, LocalizationDto>().ReverseMap();
+            CreateMap<Localization, CreateLocalizationDto>().ReverseMap();
+            CreateMap<Localization, UpdateLocalizationDto>().ReverseMap();
+        }
+    }
+}
