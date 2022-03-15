ff83345ac283ff3a49292161604d3dbd254f0916
Therion95 <piotrpawelczyk95@gmail.com>
2022-03-15 23:39:13 +0100

    New migration

---
+++ FlowMeter.Data/Migrations/20220315191752_LocalizationWithUser2.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlowMeter.Data.Migrations
+{
+    public partial class LocalizationWithUser2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shapes_Localizations_LocalizationId",
+                table: "Shapes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Shapes_LocalizationId",
+                table: "Shapes");
+
+            migrationBuilder.DropColumn(
+                name: "LocalizationId",
+                table: "Shapes");
+
+            migrationBuilder.DropColumn(
+                name: "ShapeId",
+                table: "Localizations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Localizations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Localizations_UserId",
+                table: "Localizations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Localizations_Users_UserId",
+                table: "Localizations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Localizations_Users_UserId",
+                table: "Localizations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Localizations_UserId",
+                table: "Localizations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Localizations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LocalizationId",
+                table: "Shapes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShapeId",
+                table: "Localizations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shapes_LocalizationId",
+                table: "Shapes",
+                column: "LocalizationId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shapes_Localizations_LocalizationId",
+                table: "Shapes",
+                column: "LocalizationId",
+                principalTable: "Localizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
