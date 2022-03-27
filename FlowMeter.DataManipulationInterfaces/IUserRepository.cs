9ee3f07fa1de492b607e35f0a8344d62680a68be
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-03-27 11:53:21 +0200

    IUserRepository refactor

--- FlowMeter.DataManipulationInterfaces/IUserRepository.cs
+++ FlowMeter.DataManipulationInterfaces/IUserRepository.cs
@@ -6,16 +6,9 @@ using System.Linq.Expressions;
 
 namespace FlowMeter.DataManipulationInterfaces
 {
-    public interface IUserRepository
+    public interface IUserRepository : IGenericRepository<User>
     {
         List<User> GetAllUsersWithDevicesAndLocalizations(Expression<Func<User, bool>> expression = null, Func<IQueryable<User>, IOrderedQueryable<User>> orderBy = null);
-        void Add(User entity);
-        void AddRange(IEnumerable<User> entities);
-        User Get(Expression<Func<User, bool>> expression = null, List<string> includes = null);
-        List<User> GetAll(Expression<Func<User, bool>> expression = null, Func<IQueryable<User>, IOrderedQueryable<User>> orderBy = null, List<string> includes = null);
-        void Modify(User entity);
-        void Remove(int id);
-        void RemoveRange(IEnumerable<User> entities);
         User GetByEmail(string email);
         User GetById(int id);
     }
